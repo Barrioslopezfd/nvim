65ebb83fad7f668551e64531c5c8aeb44b3d6a21
Barrioslopezfd <barrioslopezfd@gmail.com>
2025-04-24 10:52:01 -0300

    fix: lsp not attaching

--- lua/config/plugins/mason_lsp.lua
+++ lua/config/plugins/mason_lsp.lua
@@ -1,35 +1,50 @@
 return {
-    {
-        "williamboman/mason.nvim",
-        dependencies = {
-        "williamboman/mason-lspconfig.nvim",
-        "neovim/nvim-lspconfig",
-        {
-            "folke/lazydev.nvim",
-            ft = "lua", -- only load on lua files
-            opts = {
-              library = {
-                -- See the configuration section for more details
-                -- Load luvit types when the `vim.uv` word is found
-                { path = "${3rd}/luv/library", words = { "vim%.uv" } },
-              },
-            },
+  {
+    "williamboman/mason.nvim",
+    dependencies = {
+      "williamboman/mason-lspconfig.nvim",
+      "neovim/nvim-lspconfig",
+      {
+        "folke/lazydev.nvim",
+        ft = "lua", -- only load on lua files
+        opts = {
+          library = {
+            -- See the configuration section for more details
+            -- Load luvit types when the `vim.uv` word is found
+            { path = "${3rd}/luv/library", words = { "vim%.uv" } },
           },
         },
+      },
+    },
 
-        config = function()
-            require("mason").setup()
-            require("mason-lspconfig").setup()
+    config = function()
+      require("mason").setup()
+      require("mason-lspconfig").setup()
 
-            local lspconfig = require("lspconfig")
+      local lspconfig = require("lspconfig")
 
-            require("mason-lspconfig").setup_handlers({
-              function(server_name)
-                lspconfig[server_name].setup({})
-              end,
-            })
+      require("mason-lspconfig").setup_handlers({
+        function(server_name)
+          lspconfig[server_name].setup({})
+        end,
+      })
 
-        end
-    }
+      vim.api.nvim_create_autocmd("BufWritePre", {
+        pattern = "*",
+        callback = function()
+          vim.lsp.buf.format({ async = false })
+        end,
+      })
+
+      --[[ ======= LUA ======= ]] --
+      vim.api.nvim_create_autocmd("FileType", {
+        pattern = "lua",
+        callback = function()
+          vim.opt_local.shiftwidth = 2
+          vim.opt_local.tabstop = 2
+          vim.opt_local.expandtab = true
+        end,
+      })
+    end
+  }
 }
-
